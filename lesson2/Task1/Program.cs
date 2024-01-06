46fd3b65cf8274b574cea058abaa53e49f34c933
Nikolay Noskov <royal_nikolos@mail.ru>
2024-01-06 13:09:55 +1000

    Решение Task1

--- lesson2/Task1/Program.cs
+++ lesson2/Task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+ 
+        if (number % 7 == 0 && number % 23 == 0)
+        {
+            Console.WriteLine("Число кратно и 7, и 23.");
+        }
+        else if (number % 7 == 0)
+        {
+            Console.WriteLine("Число кратно только 7.");
+        }
+        else if (number % 23 == 0)
+        {
+            Console.WriteLine("Число кратно только 23.");
+        }
+        else
+        {
+            Console.WriteLine("Число не кратно ни 7, ни 23.");
+        }
+        
+        Console.ReadLine();

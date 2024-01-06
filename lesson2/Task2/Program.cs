ce38224f8c8d98f357c30082b9ec11fbf4c996de
Nikolay Noskov <royal_nikolos@mail.ru>
2024-01-06 13:14:05 +1000

    Добавлен Task4

--- lesson2/Task2/Program.cs
+++ lesson2/Task2/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
--- lesson2/Task3/Program.cs
+++ lesson2/Task3/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
